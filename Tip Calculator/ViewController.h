794b1314ecb6b5314eebfbb495c903626adc1c20
zak123 <zacharym123@gmail.com>
2015-03-20 11:46:49 -0700

    Initial Commit

---
+++ Tip Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calculator
+//
+//  Created by Zachary Mallicoat on 3/20/15.
+//  Copyright (c) 2015 cghcapital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tip Calculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Tip Calculator
+//
+//  Created by Zachary Mallicoat on 3/20/15.
+//  Copyright (c) 2015 cghcapital. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
